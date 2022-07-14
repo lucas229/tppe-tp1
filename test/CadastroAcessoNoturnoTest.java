a5d6d3a84fbd302efab02b14db90750e9ffbc87a
Lucas Rodrigues <lucasro229@gmail.com>
2022-07-14 20:53:41 -0300

    Parametrização: Cálculo do valor de acesso para acesso noturno
    Co-authored-by: Kleidson Alves      <kleidsonalves15@gmail.com>

---
+++ test/CadastroAcessoNoturnoTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+
+@RunWith(Parameterized.class)
+public class CadastroAcessoNoturnoTest {
+
+    private String placa;
+    private String horaEntrada;
+    private String horaSaida;
+    private Estacionamento estacionamento;
+    private float resultado;
+
+	@Parameters
+	public static List<Object[]> getParameters() {
+        Estacionamento estacionamento1 = new Estacionamento("Estac. 1", 30, 15, 120, 45, "19:00", "8:00", 600, 50, "6:00", "22:00", 300, 50);
+        Estacionamento estacionamento2 = new Estacionamento("Estac. 2", 20, 10, 70, 30, "21:00", "9:00", 455, 60, "0:00", "23:59", 120, 60);
+        Estacionamento estacionamento3 = new Estacionamento("Estac. 3", 10, 0, 50, 40, "20:00", "8:00", 350, 40, "6:00", "22:00", 600, 70);
+		Object[][] parameters = new Object[][] {
+			{"ABC123", "19:00", "19:30", estacionamento1, 54},
+			{"DEF456", "21:00", "9:00", estacionamento2, 21},
+			{"DEF456", "20:30", "23:59", estacionamento3, 20}
+		};
+
+		return Arrays.asList(parameters);
+	}
+
+    public CadastroAcessoNoturnoTest(String placa, String horaEntrada, String horaSaida, Estacionamento estacionamento, float resultado) {
+        this.placa = placa;
+        this.horaEntrada = horaEntrada;
+        this.horaSaida = horaSaida;
+        this.estacionamento = estacionamento;
+        this.resultado = resultado;
+    }
+
+    @Test
+    public void testCadastroAcessoNoturno() {
+        assertEquals(resultado, estacionamento.cadastrarAcesso(placa, horaEntrada, horaSaida), 0.01);
+    }
+
+}
