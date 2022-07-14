90c0d881ed2a4e6815a8e88137a4b396be0522a1
Lucas Rodrigues <lucasro229@gmail.com>
2022-07-14 13:55:26 -0300

    Parametrização: Calculo do valor de acesso para acesso de hora cheia
    Co-authored-by: Kleidson Alves      <kleidsonalves15@gmail.com>

--- test/CadastroHoraCheiaTest.java
+++ test/CadastroHoraCheiaTest.java
@@ -1,30 +1,49 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class CadastroHoraCheiaTest {
 
-    Estacionamento estacionamento;
+    private Estacionamento estacionamento;
+    private String placa;
+    private String horaEntrada;
+    private String horaSaida;
+    private float resultado;
+
+	@Parameters
+	public static List<Object[]> getParameters() {
+		Object[][] parameters = new Object[][] {
+			{"HI139", "8:30", "9:30", 72},
+			{"HI139", "8:30", "10:30", 144},
+			{"HI139", "8:30", "11:30", 216}
+		};
+
+		return Arrays.asList(parameters);
+	}
 
     @Before
     public void setup() {
         estacionamento = new Estacionamento("Estac. 2", 20, 10, 70, 30, "21:00", "7:00", 455, 60, "0:00", "23:59", 120, 60);
     }
 
+	public CadastroHoraCheiaTest(String placa, String horaEntrada, String horaSaida, float resultado) {
+		this.placa = placa;
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+		this.resultado = resultado;
+	}
+   
     @Test
-    public void testCadastroUmaHoraCheia() {
-        assertEquals(72, estacionamento.cadastrarAcesso("HI139", "8:30", "9:30"), 0.01);
-    }
-
-    @Test
-    public void testCadastroDuasHorasCheias() {
-        assertEquals(144, estacionamento.cadastrarAcesso("HI139", "8:30", "10:30"), 0.01);
-    }
-
-    @Test
-    public void testCadastroTresHorasCheias() {
-        assertEquals(216, estacionamento.cadastrarAcesso("HI139", "8:30", "11:30"), 0.01);
+    public void testCadastroHoraCheia() {
+        assertEquals(resultado, estacionamento.cadastrarAcesso(placa, horaEntrada, horaSaida), 0.01);
     }
 
 }
