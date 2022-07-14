3b27db29ad0b0a3a917bce07da235c7fa554f384
Lucas Rodrigues <lucasro229@gmail.com>
2022-07-14 14:32:57 -0300

    Duplicação: Cálculo do valor de acesso para acesso noturno
    Co-authored-by: Kleidson Alves      <kleidsonalves15@gmail.com>

--- src/Estacionamento.java
+++ src/Estacionamento.java
@@ -33,17 +33,16 @@ public class Estacionamento {
     }
 
     public float cadastrarAcesso(String placa, String horaEntrada, String horaSaida) {
-        int parsedHoraEntrada = Integer.parseInt(horaEntrada.split(":")[0]);
-        int parsedHoraSaida = Integer.parseInt(horaSaida.split(":")[0]);
-        
-        if(parsedHoraEntrada >= 19 || parsedHoraSaida <= 8) {
-            return 120 * 45 / 100;
-        }
-
         int minutosEntrada = Integer.parseInt(horaEntrada.split(":")[0]) * 60 + Integer.parseInt(horaEntrada.split(":")[1]);
         int minutosSaida = Integer.parseInt(horaSaida.split(":")[0]) * 60 + Integer.parseInt(horaSaida.split(":")[1]);
         int fracoes = (minutosSaida - minutosEntrada) / 15;
-
+        
+        if(minutosEntrada >= 21 * 60) {
+            return 70 * 30 / 100;
+        } else if(minutosEntrada >= 19 * 60) {
+            return 120 * 45 / 100;
+        }
+    
         return fracoes * valorFracao * (1 - (valorHoraCheia / 100));
     }
 }
--- test/CadastroAcessoNoturno.java
+++ test/CadastroAcessoNoturno.java
@@ -6,15 +6,22 @@ import org.junit.Test;
 public class CadastroAcessoNoturno {
     
     private Estacionamento estacionamento1;
+    private Estacionamento estacionamento2;
 
     @Before
     public void setup() {
         estacionamento1 = new Estacionamento("Estac. 1", 30, 15, 120, 45, "19:00", "8:00", 600, 50, "6:00", "22:00", 300, 50);
+        estacionamento2 = new Estacionamento("Estac. 2", 20, 10, 70, 30, "21:00", "9:00", 455, 60, "0:00", "23:59", 120, 60);
     }
 
     @Test
     public void testCadastroAcessoNoturno1() {
         assertEquals(54, estacionamento1.cadastrarAcesso("ABC123", "19:00", "19:30"), 0.01);
+    }
+
+    @Test
+    public void testCadastroAcessoNoturno2() {
+        assertEquals(21, estacionamento2.cadastrarAcesso("DEF456", "21:00", "9:00"), 0.01);
     }
 
 }
